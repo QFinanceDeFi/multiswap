bc2f53c2c5bbb2e42b5d32d34d2c22787599d813
dperk6 <daniel@perklin.ca>
2021-09-11 16:04:54 -0400

    config

--- truffle-config.js
+++ truffle-config.js
@@ -2,29 +2,12 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { mnemonic, projectId, etherscanApi, address } = require('./secrets.json');
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     develop: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: 1,       // Any network (default: none)
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 1,
     },
-    // Kovan Ethereum testnet blockchain
     kovan: {
       provider: new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${projectId}`),
       port: 443,
@@ -37,7 +20,7 @@ module.exports = {
       network_id: "1",
       from: address,
       gas: 5200000,
-      gasPrice: 155000000000
+      gasPrice: 100000000000
     }
   },
   plugins: [
@@ -46,31 +29,17 @@ module.exports = {
   api_keys: {
     etherscan: etherscanApi
   },
-  // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
   },
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.7",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.8.7",
         optimizer: {
           enabled: true,
           runs: 200
         },
-      //  evmVersion: "byzantium"
-      // }
     }
   },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
-  //
-  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
-  // those previously migrated contracts available in the .db directory, you will need to run the following:
-  // $ truffle migrate --reset --compile-all
-
   db: {
     enabled: false
   }
